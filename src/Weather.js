5346505e786dfea155a4e80757e53efe52ef32d3
bocai <xiaobo.huang@hand-china.com>
2017-09-24 15:56:58 +0800

    smart weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import './index.css'
 
 const cityCode = '101010100'
 
@@ -6,29 +7,67 @@ class Weather extends Component{
   constructor(){
     super(...arguments)
     this.state = {weather: null}
+    this.onSelectChange = this.onSelectChange.bind(this)
+    this.getWeather = this.getWeather.bind(this)
   }
-  componentDidMount(){
+  onSelectChange = (event) => {
+    let cityCode = event.target.value
+    cityCode === "" ? null : cityCode
+    if(cityCode){
+        this.getWeather(cityCode)
+    }else{
+      this.setState(...this.state, {weather: null})
+    }
+  }
+
+  getWeather = (cityCode) => {
+    // alert(this.state.cityCode)
     const apiUrl = `/data/cityinfo/${cityCode}.html`
     fetch(apiUrl).then((response) => {
       if(response.status !== 200){
         throw new Error('Fail to get response with status ' + response.status)
       }
       response.json().then((responseJson) => {
-        this.setState({weather: responseJson.weatherinfo})
+        this.setState(...this.state, {weather: responseJson.weatherinfo})
       }).catch((error) => {
-        this.setState({weather: null})
+        this.setState(...this.state, {weather: null})
       })}
     ).catch((error) => {
-      this.setState({weather: null})
+      this.setState(...this.state, {weather: null})
     })
   }
   render(){
-    if(!this.state.weather){
-      return <div>暂无数据</div>;
+    console.log(this.state)
+    var weatherInfo = ""
+    if(this.state.weather){
+        const {city, weather, temp1, temp2} = this.state.weather
+        weatherInfo = `${city} ${weather} 最高气温 ${temp2} 最低气温 ${temp1}`
+    }else{
+       weatherInfo = "暂无数据"
     }
-    const {city, weather, temp1, temp2} = this.state.weather;
-    return(
-      <div> {weather} 最高气温 {temp2} 最低气温 {temp1}</div>
+    return (
+      <div className='weather-container'>
+       <div className='select-city'>
+         <div className='label'>城市:</div>
+         <div>
+           <select className="city-selector" onChange={this.onSelectChange}>
+             <option value="">请选择城市</option>
+             <option value="101010100">北京</option>
+             <option value="101020100">上海</option>
+             <option value="101030100">天津</option>
+             <option value="101040100">重庆</option>
+             <option value="101050101">哈尔滨</option>
+             <option value="101250101">长沙</option>
+             <option value="101251201">怀化</option>
+             <option value="101260101">贵阳</option>
+           </select>
+         </div>
+        </div>
+        <div className='weather-result'>
+          <div className='label'>天气:</div>
+          <div>{weatherInfo}</div>
+        </div>
+      </div>
     )
   }
 }
